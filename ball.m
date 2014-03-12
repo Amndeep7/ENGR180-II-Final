babfb822976d5cee60fda7b76236b73277656bd9
Amndeep Singh Mann <amndeep.dev@gmail.com>
2014-03-12 06:49:32 -0400

    a lot of stuff
    accurate 2d collision physics, graphics, some user input but not stick
    yet

--- ball.m
+++ ball.m
@@ -14,7 +14,7 @@ classdef ball < handle
         %specifications as according to american pool
         mass=170; %170 grams
         radius=2.25/12; %2 1/4 inches in feet
-        friction=0.001;
+        friction=0.005;
     end
     
     methods
@@ -22,63 +22,88 @@ classdef ball < handle
             obj.position = position;
             obj.velocity = velocity;
         end
+    end
+    
+    methods (Static = true)
+        function inpocket = ball_in_pocket(one, x, y, r)
+            inpocket = (one.position(1)-x)^2+(one.position(2)-y)^2 <= (ball.radius+r)^2;
+        end
+        
         
         %an intersection between two circles occurs when the distance
         %between the centers is not greater than the sum of their radii
-        function intersected = ball_intersection(obj, other)
-            intersected = (obj.position(1)-other.position(1))^2+(obj.position(2)-other.position(2))^2 <= (2*ball.radius)^2;
+        function intersected = ball_intersection(one, two)
+            intersected = (one.position(1)-two.position(1))^2+(one.position(2)-two.position(2))^2 <= (2*ball.radius)^2;
+        end
+        
+        function ball_collision(one, two)
+            one_angle = atan2(one.velocity(2), one.velocity(1));
+            two_angle = atan2(two.velocity(2), two.velocity(1));
+            one_speed = sum(one.velocity.*one.velocity)^0.5;
+            two_speed = sum(two.velocity.*two.velocity)^0.5;
+            collision_angle = atan((two.position(2)-one.position(2))/(two.position(1)-one.position(1)));
+            one_new_x = two_speed*cos(two_angle-collision_angle)*cos(collision_angle)+one_speed*sin(one_angle-collision_angle)*cos(collision_angle+pi/2);
+            one_new_y = two_speed*cos(two_angle-collision_angle)*sin(collision_angle)+one_speed*sin(one_angle-collision_angle)*sin(collision_angle+pi/2);
+            two_new_x = one_speed*cos(one_angle-collision_angle)*cos(collision_angle)+two_speed*sin(two_angle-collision_angle)*cos(collision_angle+pi/2);
+            two_new_y = one_speed*cos(one_angle-collision_angle)*sin(collision_angle)+two_speed*sin(two_angle-collision_angle)*sin(collision_angle+pi/2);
+            one.velocity = [one_new_x, one_new_y, one.velocity(3)];
+            two.velocity = [two_new_x, two_new_y, two.velocity(3)];
         end
         
         %simulates the movement of one of the balls
-        function move(obj, time_increment, minx, miny, maxx, maxy, balls)
-            %update position
-            obj.position = obj.position + time_increment*obj.velocity;
-            
-            %check if the position goes beyond a wall boundary
-            if obj.position(1)-ball.radius < minx
-                obj.position(1) = minx+ball.radius;
-                obj.velocity(1) = -obj.velocity(1);
-            elseif obj.position(1)+ball.radius > maxx
-                obj.position(1) = maxx-ball.radius;
-                obj.velocity(1) = -obj.velocity(1);
-            end
-            if obj.position(2)-ball.radius < miny
-                obj.position(2) = miny+ball.radius;
-                obj.velocity(2) = -obj.velocity(2);
-            elseif obj.position(2)+ball.radius > maxy
-                obj.position(2) = maxy-ball.radius;
-                obj.velocity(2) = -obj.velocity(2);
-            end
-            
-            %check if there's been a ball-to-ball collision, whereupon
-            %apply the physics of an elastic collision - simplified to the
-            %case where masses are equivalent
-            for other = balls
-                if obj.ball_intersection(other)
-                    new_self_velocity = other.velocity;
-                    other.velocity = obj.velocity;
-                    obj.velocity = new_self_velocity;
+        function move(time_increment, minx, miny, maxx, maxy, balls)
+            for b1 = 1:length(balls)
+                %update position
+                balls(b1).position = balls(b1).position + time_increment*balls(b1).velocity;
+
+                %check if the position goes beyond a wall boundary
+                if balls(b1).position(1)-ball.radius < minx
+                    balls(b1).position(1) = minx+ball.radius;
+                    balls(b1).velocity(1) = -balls(b1).velocity(1);
+                elseif balls(b1).position(1)+ball.radius > maxx
+                    balls(b1).position(1) = maxx-ball.radius;
+                    balls(b1).velocity(1) = -balls(b1).velocity(1);
                 end
-            end
-            
-            %applies friction
-            if obj.velocity(1)^2 ~= 0
-                if obj.velocity(1)^2 < 0.005
-                    obj.velocity(1) = 0;
-                elseif obj.velocity(1) > 0
-                    obj.velocity(1) = obj.velocity(1)-ball.friction;
-                else
-                    obj.velocity(1) = obj.velocity(1)+ball.friction;
+                if balls(b1).position(2)-ball.radius < miny
+                    balls(b1).position(2) = miny+ball.radius;
+                    balls(b1).velocity(2) = -balls(b1).velocity(2);
+                elseif balls(b1).position(2)+ball.radius > maxy
+                    balls(b1).position(2) = maxy-ball.radius;
+                    balls(b1).velocity(2) = -balls(b1).velocity(2);
                 end
-            end
-            if obj.velocity(2)^2 ~= 0
-                if obj.velocity(2)^2 < 0.005
-                    obj.velocity(2) = 0;
-                elseif obj.velocity(2) > 0
-                    obj.velocity(2) = obj.velocity(2)-ball.friction;
-                else
-                    obj.velocity(2) = obj.velocity(2)+ball.friction;
+
+                %check if there's been a ball-to-ball collision, whereupon
+                %apply the physics of an elastic collision - simplified to the
+                %case where masses are equivalent
+                for b2 = b1+1:length(balls)
+                    if ball.ball_intersection(balls(b1), balls(b2))
+%                         new_self_velocity = balls(b2).velocity;
+%                         balls(b2).velocity = balls(b1).velocity;
+%                         balls(b1).velocity = new_self_velocity;
+                        ball.ball_collision(balls(b1), balls(b2))
+                    end
                 end
+
+                %applies friction
+                if balls(b1).velocity(1)^2 ~= 0
+                    if balls(b1).velocity(1)^2 < 0.005
+                        balls(b1).velocity(1) = 0;
+                    elseif balls(b1).velocity(1) > 0
+                        balls(b1).velocity(1) = balls(b1).velocity(1)-ball.friction;
+                    else
+                        balls(b1).velocity(1) = balls(b1).velocity(1)+ball.friction;
+                    end
+                end
+                if balls(b1).velocity(2)^2 ~= 0
+                    if balls(b1).velocity(2)^2 < 0.005
+                        balls(b1).velocity(2) = 0;
+                    elseif balls(b1).velocity(2) > 0
+                        balls(b1).velocity(2) = balls(b1).velocity(2)-ball.friction;
+                    else
+                        balls(b1).velocity(2) = balls(b1).velocity(2)+ball.friction;
+                    end
+                end
+
             end
             
         end
--- finalproject.m
+++ finalproject.m
@@ -11,38 +11,97 @@ hold on;
 xlabel('x');
 ylabel('y');
 zlabel('z');
+
 x = [0, 9.32, 9.32, 0, 0];
 y = [0, 0, 4.65, 4.65, 0];
 
-fill(x,y,[56/255 179/255 38/255]);
+table = imread('board.png');
+surf([0, 9.32; 0, 9.32], [0, 0; 4.65, 4.65], [0, 0; 0, 0], 'CData', table, 'FaceColor', 'texturemap');
+
+disp('You will be asked to provide certain input, please make sure to keep everything in the appropriate bounds.');
+disp('The table has the dimensions of 9.32''x4.65'', so please make sure the balls are well within them.');
+disp('Try not to stretch reality too far with the velocities of the balls.');
+ball8 = ball([input('x-pos of the 8 ball: '), input('y-pos of the 8 ball: '), ball.radius], [input('vx of the 8 ball: '), input('vy of the 8 ball: '), 0]);
+ballc = ball([input('x-pos of the cue ball: '), input('y-pos of the cue ball: '), ball.radius], [input('vx of the cue ball: '), input('vy of the cue ball: '), 0]);
+balls = [ball8, ballc];
+
+ball8_graphics_rgb = imread('magic 8 ball 2.png');
+[ball8_ind, ball8_map] = rgb2ind(ball8_graphics_rgb, 256);
+ballc_graphics_rgb = imread('drexel logo.jpg');
+[ballc_ind, ballc_map] = rgb2ind(ballc_graphics_rgb, 256);
+
+colormap([ball8_map; ballc_map]);
 
 [xsph, ysph, zsph] = sphere(20);
 
-cueball = ball([2, 2, ball.radius], [0, 0, 0]);
-otherball = ball([4, 2, ball.radius], [-10, 0, 0]);
+ball8_handle = surface(xsph, ysph, zsph, flipud(ball8_ind), 'FaceColor', 'texturemap', 'EdgeColor', 'none', 'CDataMapping', 'direct');
+ballc_handle = surface(xsph, ysph, zsph, flipud(ballc_ind), 'FaceColor', 'texturemap', 'EdgeColor', 'none', 'CDataMapping', 'direct');
 
-balls = [cueball, otherball];
+set(ballc_handle, 'CData', get(ballc_handle, 'CData')+length(ball8_map));
 
-cueball_handle = surface(xsph,ysph,zsph);
-otherball_handle = surface(xsph,ysph,zsph);
-
-cueball_hg = hgtransform('parent', someAxes);
-set(cueball_handle, 'parent', cueball_hg);
-otherball_hg = hgtransform('parent', someAxes);
-set(otherball_handle, 'parent', otherball_hg);
+ball8_hg = hgtransform('parent', someAxes);
+set(ball8_handle, 'parent', ball8_hg);
+ballc_hg = hgtransform('parent', someAxes);
+set(ballc_handle, 'parent', ballc_hg);
 drawnow
-
-while sum([balls.velocity].^2) ~= 0
-    cueball_translation = makehgtform('translate', cueball.position);
-    cueball_scaling = makehgtform('scale', cueball.radius);
-    set(cueball_hg, 'matrix', cueball_translation*cueball_scaling);
-    otherball_translation = makehgtform('translate', otherball.position);
-    otherball_scaling = makehgtform('scale', otherball.radius);
-    set(otherball_hg, 'matrix', otherball_translation*otherball_scaling);
+ball8_angle_change = 0;
+ballc_angle_change = 0;
+while (ball8.isvalid() || ballc.isvalid()) && sum([balls.velocity].^2) ~= 0
+    balls_in_simulation = {};
     
-    cueball.move(0.001, 0, 0, 9.32, 4.65, otherball);
-    otherball.move(0.001, 0, 0, 9.32, 4.65, cueball);
+    if ball8.isvalid()
+        ball8_translation = makehgtform('translate', ball8.position);
+        ball8_scaling = makehgtform('scale', ball8.radius);
+        ball8_rotating = makehgtform('axisrotate', [-ball8.velocity(2), ball8.velocity(1), ball8.velocity(3)], ball8_angle_change);
+        if(sum(ball8.velocity) == 0)
+            ball8_rotating = [1, 0, 0, 0; 0, 1, 0, 0; 0, 0, 1, 0; 0, 0, 0, 1];
+        end
+        set(ball8_hg, 'matrix', ball8_translation*ball8_scaling*ball8_rotating);
+        balls_in_simulation{length(balls_in_simulation)+1} = ball8;
+    end
+    
+    if ballc.isvalid()
+        ballc_translation = makehgtform('translate', ballc.position);
+        ballc_scaling = makehgtform('scale', ballc.radius);
+        ballc_rotating = makehgtform('axisrotate', [-ballc.velocity(2), ballc.velocity(1), ballc.velocity(3)], ballc_angle_change);
+        if(sum(ballc.velocity) == 0)
+            ballc_rotating = [1, 0, 0, 0; 0, 1, 0, 0; 0, 0, 1, 0; 0, 0, 0, 1];
+        end
+        set(ballc_hg, 'matrix', ballc_translation*ballc_scaling*ballc_rotating);
+        balls_in_simulation{length(balls_in_simulation)+1} = ballc;
+    end
+    
+    ball.move(0.001, 0, 0, 9.32, 4.65, [balls_in_simulation{:}]);
+    
+    for xpocket = [0, 9.32/2, 9.32]
+        for ypocket = [0, 4.65]
+            if ball8.isvalid() && ball.ball_in_pocket(ball8, xpocket, ypocket, 0.1)
+                delete(ball8)
+                set(ball8_handle, 'visible', 'off');
+                if ballc.isvalid()
+                    balls = ballc;
+                else
+                    balls = [];
+                end
+            end
+            if ballc.isvalid() && ball.ball_in_pocket(ballc, xpocket, ypocket, 0.1)
+                delete(ballc)
+                set(ballc_handle, 'visible', 'off');
+                if ball8.isvalid()
+                    balls = ball8;
+                else
+                    balls = [];
+                end
+            end
+        end
+    end
+    
+    if ball8.isvalid()
+        ball8_angle_change = ball8_angle_change+0.003*sum(ball8.velocity.*ball8.velocity)^0.5;
+    end
+    if ballc.isvalid()
+        ballc_angle_change = ballc_angle_change+0.003*sum(ballc.velocity.*ballc.velocity)^0.5;
+    end
     
     pause(0.015);
-    a = sum([balls.velocity].^2)
 end
